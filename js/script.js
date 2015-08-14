a3c186cf5480431afba903a9cdcbe15f247d7905
mizukmb <mizukmb6@gmail.com>
2015-08-14 10:29:06 +0900

    get qiita stocks at Qiita API

--- js/script.js
+++ js/script.js
@@ -7,7 +7,26 @@ document.body.onload = function() {
 
       var target = document.getElementById("ires");
       target.parentNode.insertBefore(ele, target);
+      console.log(getQiitaData("mizukmb"));
+      getQiitaData("mizukmb");
     } else {
     }
   });
 }
+
+function getQiitaData(username) {
+  // https://qiita.com/api/v2/docs#get-apiv2usersuser_idstocks
+  var apiURL = "//qiita.com/api/v2/users/" + username + "/stocks?page=1&per_page=1";
+  xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() {
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
+      var data = JSON.parse(this.responseText);
+      console.log(data[0].title);
+      console.log(data[0].url);
+    }
+  }
+  xmlHttp.open("GET", apiURL, true);
+
+  xmlHttp.send(null);
+}
+
