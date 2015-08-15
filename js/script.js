ebe7bc914f95d35001679570a68d0d8a7e65bbab
mizukmb <mizukmb6@gmail.com>
2015-08-15 18:34:40 +0900

    display searched qiita stock in google search result

--- js/script.js
+++ js/script.js
@@ -1,20 +1,28 @@
 document.body.onload = function() {
   chrome.storage.sync.get(["hatena", "qiita"], function(items) {
     if(items.hatena != null || items.qiita != null) {
-      var ele = document.createElement("div");
-      ele.setAttribute("id", "hateqii");
-      ele.innerHTML = "<p>Hatena ID:" + items.hatena + "</p>" + "<p>QiitaID:" + items.qiita + "</p>";
+      var qiitaJSONData = JSON.parse(ajax("//qiita.com/api/v2/users/" + items.qiita + "/stocks?par_page=100"));
 
-      var target = document.getElementById("ires");
-      target.parentNode.insertBefore(ele, target);
-      var qiitaJSONData = JSON.parse(ajax("//qiita.com/api/v2/users/" + items.qiita + "/stocks"));
       var googleSearchWords = document.getElementById("lst-ib").value.replace(/　/g, " ").split(" ");
-      console.log(googleSearchWords);
       var qiitaStockTitles = [];
       for(var i=0; i<qiitaJSONData.length; i++) {
         qiitaStockTitles.push(qiitaJSONData[i].title);
       }
-    } else {
+
+      var titles = searchQiitaStocks(googleSearchWords, qiitaStockTitles);
+
+      var ele = document.createElement("div");
+      ele.setAttribute("id", "hateqii");
+      ele.innerHTML = "";
+      for(var i=0; i<titles.length; i++) {
+        for(var j=0; j<qiitaJSONData.length; j++) {
+          if(titles[i] == qiitaJSONData[j].title) {
+            ele.innerHTML = ele.innerHTML + "<p><a href=\"" + qiitaJSONData[j].url + "\">" + titles[i] + "</p>";
+          }
+        }
+      }
+      var target = document.getElementById("ires");
+      target.parentNode.insertBefore(ele, target);
     }
   });
 }
@@ -32,13 +40,15 @@ function searchQiitaStocks(words, titles) {
     var re = new RegExp(words[i], "i");
     for(var j=0; j<titles.length; j++) {
       if(titles[j].match(re)) {
-        console.log(titles[j]);
-        console.log(words[i]);
         matchTitles.push(titles[j]);
       }
     }
+    if(i>0) {
+      matchTitles = matchTitles.filter(function(x,i,self) {
+        return self.indexOf(x) === i && i !== self.lastIndexOf(x);
+      });
+    }
   }
-  return matchTitles.filter(function (x, i, self) {
-    return self.indexOf(x) === i;
-  });
+
+  return matchTitles;
 }
